ca43f8eb9b73766e08db2fd499bee158ac1de962
hkue0610 <heiko.kuehner@mail.schwarz>
2024-02-12 09:19:46 +0100

    Apply improvements.

--- src/main/java/it/schwarz/jobs/review/coupon/api/ApplyCouponRequestDto.java
+++ src/main/java/it/schwarz/jobs/review/coupon/api/ApplyCouponRequestDto.java
@@ -2,10 +2,12 @@ package it.schwarz.jobs.review.coupon.api;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ApplyCouponRequestDto(
         @Valid
+        @NotNull
         BasketDto basket,
 
         @NotBlank
